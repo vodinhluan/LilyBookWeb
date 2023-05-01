3e847db9b05a724ad3f7aaf8139faa1ad82d3c4f
gameoflord012 <manhhieu012@gmail.com>
2023-05-01 17:16:01 +0700

    final

---
+++ LilyBook.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,26 @@
+﻿using LilyBook.DataAccess.Repository.IRepository;
+using LilyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
---
+++ LilyBook.Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+
+namespace LilyBook.Models;
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
