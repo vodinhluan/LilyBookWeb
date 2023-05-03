4624bc30a9280cfb0d3a46c5c82bdabd7b8b3656
gameoflord012 <manhhieu012@gmail.com>
2023-05-03 15:25:16 +0700

    final_1

--- LilyBook.Models/ApplicationUser.cs
+++ LilyBook.Models/ApplicationUser.cs
@@ -10,17 +10,4 @@ using System.Threading.Tasks;
 
 namespace LilyBook.Models
 {
-    public class ApplicationUser : IdentityUser
-    {
-        [Required]
-        public string Name { get; set; }
-        public string? StreetAddress { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? PostalCode { get; set; }
-        public int? CompanyId { get; set; }
-        [ForeignKey("CompanyId")]
-        [ValidateNever]
-        public Company Company { get; set; }
-    }
 }
